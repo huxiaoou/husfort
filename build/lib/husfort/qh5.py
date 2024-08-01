1e5bf8e491c803b7c5f6664f575ba2862252eadb
huxiaoou <516984451@qq.com>
2024-08-01 17:09:01 +0800

    Add CDbHDF5PlusTDates

--- build/lib/husfort/qh5.py
+++ build/lib/husfort/qh5.py
@@ -1,5 +1,8 @@
 import os
 import pandas as pd
+from loguru import logger
+from husfort.qutility import SFY
+from husfort.qcalendar import CCalendar
 
 
 class CDbHDF5:
@@ -17,6 +20,10 @@ class CDbHDF5:
     @property
     def path(self) -> str:
         return os.path.join(self.db_save_dir, self.db_name)
+
+    @property
+    def full_name(self) -> str:
+        return os.path.join(self.db_save_dir, self.db_name, self.table)
 
     def query_all(self) -> pd.DataFrame:
         with pd.HDFStore(self.path, mode="r") as store:
@@ -40,7 +47,55 @@ class CDbHDF5:
             df: pd.DataFrame = store.select(key=self.table, where=conds)  # type:ignore
         return df
 
+    def head(self, n: int = 5) -> pd.DataFrame:
+        with pd.HDFStore(self.path, mode="r") as store:
+            df: pd.DataFrame = store.select(key=self.table, start=0, stop=n)  # type:ignore
+        return df
+
+    def tail(self, n: int = 5) -> pd.DataFrame:
+        with pd.HDFStore(self.path, mode="r") as store:
+            df: pd.DataFrame = store.select(key=self.table, start=-n, stop=None)  # type:ignore
+        return df
+
     def append(self, df: pd.DataFrame, data_columns: list[str] | bool = None):
         with pd.HDFStore(self.path, mode="a") as store:
             store.append(key=self.table, value=df, format="table", data_columns=data_columns)
         return 0
+
+
+class CDbHDF5PlusTDates(CDbHDF5):
+    """
+        This class would assume it has a column named "trade_date", and type = str
+        Data in this File must be continuous in the daily scale
+    """
+
+    def check_continuity(self, append_date: str, calendar: CCalendar) -> int:
+        try:
+            rows = self.tail(1)
+        except (FileNotFoundError, KeyError):
+            logger.warning(
+                f"Database {SFY(self.full_name)} may not exist, "
+                f"when appending new data, continuity check would assume it is continuous")
+            return 0
+        if len(rows) > 0:
+            expected_next_date = calendar.get_next_date(last_date := rows["trade_date"].iloc[0], shift=1)
+        else:
+            logger.warning(
+                f"Database {SFY(self.full_name)} does exist, but it is empty, "
+                f"when appending new data, continuity check would assume it is continuous")
+            return 0
+
+        if expected_next_date == append_date:
+            return 0
+        elif expected_next_date < append_date:
+            logger.warning(f"Warning! Last date of {SFY(self.full_name)} is {SFY(last_date)}")
+            logger.warning(f"And expected next date should be {SFY(expected_next_date)}")
+            logger.warning(f"But date to append = {SFY(append_date)}")
+            logger.warning(f"Some days may be {SFY('omitted')}")
+            return 1
+        else:  # expected_next_date > append_date
+            logger.warning(f"Warning! Last date of {SFY(self.full_name)} is {SFY(last_date)}")
+            logger.warning(f"And expected next date should be {SFY(expected_next_date)}")
+            logger.warning(f"But date to append = {SFY(append_date)}.")
+            logger.warning(f"Some days may be {SFY('overlapped')}")
+            return 2
--- build/lib/husfort/tests/test_qh5.py
+++ build/lib/husfort/tests/test_qh5.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import scipy.stats as sps
-import datetime as dt
 import random
 
 
-def create_data(nrow: int, ncol: int, cnames: list[str], start_id: int,
+def create_data(nrow: int, ncol: int, cnames: list[str], hist_dates: list[str],
                 random_state: int = None) -> pd.DataFrame:
     _data = sps.norm.rvs(size=(nrow, ncol), loc=0, scale=1, random_state=random_state)
     _df = pd.DataFrame(data=_data, columns=cnames)
-    _start_date = dt.datetime(year=2012, month=1, day=1)
-    _df["trade_date"] = [(_start_date + dt.timedelta(days=_)).strftime("%Y%m%d") for _ in range(nrow)]
+    _df["trade_date"] = hist_dates[0:nrow]
     _df["instrument"] = random.choices(population=list("abcd"), k=nrow)
     _df = _df[["trade_date"] + ["instrument"] + cnames]
     return _df
@@ -17,32 +15,42 @@ def create_data(nrow: int, ncol: int, cnames: list[str], start_id: int,
 
 if __name__ == "__main__":
     from loguru import logger
+    from husfort.qutility import SFY
+    from husfort.qcalendar import CCalendar
     from husfort.qlog import define_logger
-    from husfort.qh5 import CDbHDF5
+    from husfort.qh5 import CDbHDF5PlusTDates
 
     define_logger()
 
-    db_save_dir, db_name = r"E:\\tmp", "test.h5"
+    calendar_path = r"E:\Deploy\Data\Calendar\cne_calendar.csv"
+    calendar = CCalendar(calendar_path)
+    h_dates = calendar.get_iter_list(bgn_date="20120101", stp_date="20250101")
+
+    db_save_dir, db_name = r"E:\tmp", "test.h5"
     table_name = "grp1/grp2/testTable"
     nr, nc = 20, 5
     cnms = [f"C{_:02d}" for _ in range(nc)]
-    df = create_data(nr * 2, nc, cnames=cnms, start_id=0)
+    df = create_data(nr * 2, nc, cnames=cnms, hist_dates=h_dates)
     df_head, df_tail = df.head(nr), df.tail(nr)
 
-    h5lib = CDbHDF5(db_save_dir=db_save_dir, db_name=db_name, table=table_name)
+    h5lib = CDbHDF5PlusTDates(db_save_dir=db_save_dir, db_name=db_name, table=table_name)
 
     # --- first writing
-    h5lib.append(df=df_head, data_columns=["trade_date", "instrument"])
-    df0 = h5lib.query_all()
-    logger.info(f"The original data length = {len(df0)} ")
-    print(df0)
+    append_date = df_head["trade_date"].iloc[0]
+    if h5lib.check_continuity(append_date=append_date, calendar=calendar) == 0:
+        h5lib.append(df=df_head, data_columns=["trade_date", "instrument"])
+        df0 = h5lib.query_all()
+        logger.info(f"The original data length = {len(df0)} ")
+        print(df0)
 
     # --- appending
-    h5lib.append(df_tail)
-    logger.info("Append data to lib")
-    df0 = h5lib.query_all()
-    logger.info(f"After appending, data length = {len(df0)} ")
-    print(df0)
+    append_date = df_tail["trade_date"].iloc[0]
+    if h5lib.check_continuity(append_date=append_date, calendar=calendar) == 0:
+        h5lib.append(df_tail)
+        logger.info("Append data to lib")
+        df0 = h5lib.query_all()
+        logger.info(f"After appending, data length = {len(df0)} ")
+        print(df0)
 
     # --- query
     df1 = h5lib.query(conds=["trade_date <= '20120131'"])
@@ -56,3 +64,21 @@ if __name__ == "__main__":
     df3 = h5lib.query(conds=["instrument = 'd' | instrument = 'c'", "trade_date < '20120205'"])
     logger.info("Query: (instrument = 'd' | instrument = 'c') AND (trade_date < '20120205')")
     print(df3)
+
+    head_data = h5lib.head(10)
+    logger.info("10 head lines")
+    print(head_data)
+
+    tail_data = h5lib.tail(10)
+    logger.info("10 tail lines")
+    print(tail_data)
+
+    # --- continuity check
+    h5lib.check_continuity(append_date="20120306", calendar=calendar)
+    h5lib.check_continuity(append_date="20120307", calendar=calendar)
+    h5lib.check_continuity(append_date="20120308", calendar=calendar)
+
+    table_name_alt = "grp1/grp2/testTable2"
+    h5lib = CDbHDF5PlusTDates(db_save_dir=db_save_dir, db_name=db_name, table=table_name_alt)
+    if h5lib.check_continuity(append_date="20120301", calendar=calendar) == 0:
+        logger.info(f"Continuity checking for empty table: {SFY(h5lib.full_name)} is successful.")
