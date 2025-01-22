65ae63b97f5403db1e35555150b25dc1649bb62a
huxiaoou <516984451@qq.com>
2025-01-22 17:27:22 +0800

    Update install_to_bin.sh

--- install_to_bin.sh
+++ install_to_bin.sh
@@ -1,6 +1,11 @@
-chmod u+x src/husfort/utility/*py
 cp src/husfort/utility/get_datetime_fromtimestamp.py /usr/local/bin/get_datetime_fromtimestamp
 cp src/husfort/utility/view_colors.py /usr/local/bin/view_colors
 cp src/husfort/utility/view_csv.py /usr/local/bin/view_csv
 cp src/husfort/utility/view_h5.py /usr/local/bin/view_h5
 cp src/husfort/utility/view_sql.py /usr/local/bin/view_sql
+
+chmod o+x /usr/local/bin/get_datetime_fromtimestamp
+chmod o+x /usr/local/bin/view_colors
+chmod o+x /usr/local/bin/view_csv
+chmod o+x /usr/local/bin/view_h5
+chmod o+x /usr/local/bin/view_sql
