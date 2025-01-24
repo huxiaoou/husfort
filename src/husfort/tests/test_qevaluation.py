3470eaaa73c0c010725e36b1a1068e00f7768876
huxiaoou <516984451@qq.com>
2025-01-24 17:14:53 +0800

    Update tests.test_qevaluation

--- src/husfort/tests/test_qevaluation.py
+++ src/husfort/tests/test_qevaluation.py
@@ -1,20 +1,34 @@
 if __name__ == "__main__":
+    import argparse
     import pandas as pd
     import scipy.stats as sps
     from husfort.qevaluation import CNAV
 
-    mu, sd = 0.002, 0.012
-    n = 2000
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "-m", "--mu", type=float, default=0.002, help="average of return")
+    arg_parser.add_argument(
+        "-s", "--sigma", type=float, default=0.012, help="volatility of return")
+    arg_parser.add_argument(
+        "--size", type=int, default=2000, help="size of samples"
+    )
+    args = arg_parser.parse_args()
+
+    mu, sd = args.mu, args.sigma
+    n = args.size
     dates = [f"T{_:04d}" for _ in range(n)]
     ret = sps.norm.rvs(loc=mu, scale=sd, size=n)
 
     ret_data = pd.Series(data=ret, index=dates)
     print(ret_data)
+    print("-" * 24)
 
     nav = CNAV(input_srs=ret_data, input_type="RET")
     nav.cal_all_indicators(excluded=("var",), qs=(1, 99))
     res = nav.to_dict()
-    print(res)
+    print(pd.Series(res))
+    print("-" * 24)
 
     res_display = nav.reformat_to_display()
-    print(res_display)
+    print(pd.Series(res_display))
+    print("-" * 24)
