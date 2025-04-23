438567059d0fd3b5dbe17001965a4a15c6815ab4
huxiaoou <516984451@qq.com>
2025-04-23 17:51:46 +0800

    Add qmultiprocessing

---
+++ src/husfort/qmultiprocessing.py
@@ -0,0 +1,158 @@
+import time
+from multiprocessing import Pool, Queue, Manager
+from multiprocessing import set_start_method, get_start_method
+from rich.progress import Progress, TaskID, BarColumn, TimeRemainingColumn, TimeElapsedColumn
+from typing import Callable
+from enum import IntEnum
+from dataclasses import dataclass
+
+
+class EStatusWorker(IntEnum):
+    START = 0
+    FINISHED = 1
+
+
+class EOperationQueue(IntEnum):
+    SET_DESC = 0
+    SET_TOTAL = 1
+    SET_ADVANCE = 2
+    SET_COMPLETE = 3
+    SET_STATUS = 4
+
+
+@dataclass
+class TMsg:
+    task_id: TaskID
+    operation: EOperationQueue
+    val: int | float | str | EStatusWorker
+
+
+class CAgentQueue:
+    def __init__(self, task_id: TaskID, queue: Queue):
+        self.task_id = task_id
+        self.queue = queue
+
+    def set_description(self, desc: str):
+        msg = TMsg(self.task_id, EOperationQueue.SET_DESC, desc)
+        self.queue.put(msg)
+        return 0
+
+    def set_total(self, total: int):
+        msg = TMsg(self.task_id, EOperationQueue.SET_TOTAL, total)
+        self.queue.put(msg)
+        return 0
+
+    def set_advance(self, advance: int):
+        msg = TMsg(self.task_id, EOperationQueue.SET_ADVANCE, advance)
+        self.queue.put(msg)
+        return 0
+
+    def set_completed(self, completed: int):
+        msg = TMsg(self.task_id, EOperationQueue.SET_COMPLETE, completed)
+        self.queue.put(msg)
+        return 0
+
+    def set_status(self, status: EStatusWorker):
+        msg = TMsg(self.task_id, EOperationQueue.SET_STATUS, status)
+        self.queue.put(msg)
+        return 0
+
+
+def update_progress(pb: Progress, size: int, queue: Queue, seconds_between_check: float):
+    completed = 0
+    while completed < size:
+        if not queue.empty():
+            msg: TMsg = queue.get()
+            if msg.operation == EOperationQueue.SET_DESC:
+                pb.update(msg.task_id, description=msg.val)
+            elif msg.operation == EOperationQueue.SET_TOTAL:
+                pb.update(msg.task_id, total=msg.val)
+            elif msg.operation == EOperationQueue.SET_ADVANCE:
+                pb.update(msg.task_id, advance=msg.val)
+            elif msg.operation == EOperationQueue.SET_COMPLETE:
+                pb.update(msg.task_id, completed=msg.val)
+            elif msg.operation == EOperationQueue.SET_STATUS:
+                if msg.val == EStatusWorker.FINISHED:
+                    completed += 1
+        time.sleep(seconds_between_check)
+    return 0
+
+
+TTask = tuple[Callable, tuple]
+
+
+def mul_process_for_tasks(
+        tasks: list[TTask],
+        processes: int = None,
+        bar_width: int = 100,
+        seconds_between_check: float = 0.01,
+):
+    """
+
+    :param tasks: a TTask, the first parameter of TTask.callable must be CAgentQueue.
+                  And other parameters for the callable in TTask.tuple
+    :param processes: number of processes to use
+    :param bar_width: the width of the progress bar
+    :param seconds_between_check: time duration between checks of tasks
+    :return:
+    """
+
+    with Progress(
+            "[progress.description]{task.description}",
+            BarColumn(bar_width=bar_width),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            TimeRemainingColumn(),
+            TimeElapsedColumn(),
+    ) as pb:
+        if not get_start_method():
+            set_start_method("spawn")
+        with Manager() as manager:
+            queue = manager.Queue()
+            with Pool(processes=processes) as pool:
+                for f, args in tasks:
+                    task_id = pb.add_task(description="New Task")
+                    agent_queue = CAgentQueue(task_id, queue)
+                    pool.apply_async(
+                        f, args=(agent_queue, *args),
+                        error_callback=lambda e: print(e),
+                    )
+                pool.close()
+                update_progress(pb, len(tasks), queue, seconds_between_check)
+                pool.join()
+    return 0
+
+
+def uni_process_loop(tasks: list[TTask], task_ids: list[TaskID], queue: Queue):
+    for (f, args), task_id in zip(tasks, task_ids):
+        agent_queue = CAgentQueue(task_id, queue)
+        f(agent_queue, *args)
+    return 0
+
+
+def uni_process_for_tasks(
+        tasks: list[TTask],
+        bar_width: int = 100,
+        seconds_between_check: float = 0.01,
+):
+    with Progress(
+            "[progress.description]{task.description}",
+            BarColumn(bar_width=bar_width),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            TimeRemainingColumn(),
+            TimeElapsedColumn(),
+    ) as pb:
+        task_ids = [pb.add_task(description="New Task") for _ in range(len(tasks))]
+
+        if not get_start_method():
+            set_start_method("spawn")
+        with Manager() as manager:
+            queue = manager.Queue()
+            with Pool() as pool:
+                pool.apply_async(
+                    uni_process_loop, args=(tasks, task_ids, queue),
+                    error_callback=lambda e: print(e),
+                )
+                pool.close()
+                update_progress(pb, len(tasks), queue, seconds_between_check)
+                pool.join()
+    return 0
---
+++ src/husfort/tests/test_qmultiprocessing.py
@@ -0,0 +1,34 @@
+import time
+from random import randint
+from husfort.qmultiprocessing import CAgentQueue, EStatusWorker, TTask, mul_process_for_tasks, uni_process_for_tasks
+
+
+def worker(agent_queue: CAgentQueue, total: int, minimum_sleep: float):
+    """
+
+    :param agent_queue: the first parameter must be CAgentQueue
+    :param total: the total steps of this task
+    :param minimum_sleep: the minimum sleep time, unit = seconds
+    :return:
+    """
+
+    agent_queue.set_description(f"Task {agent_queue.task_id:>02d} Total = {total}")
+    agent_queue.set_total(total)
+    for i in range(total):
+        time.sleep(minimum_sleep * randint(a=1, b=3))
+        agent_queue.set_completed(i + 1)  # agent_queue.set_advance(1)
+    agent_queue.set_status(EStatusWorker.FINISHED)
+    return 0
+
+
+if __name__ == "__main__":
+    tasks = [
+        TTask((worker, (20, 0.1))),
+        TTask((worker, (15, 0.2))),
+        TTask((worker, (20, 0.3))),
+        TTask((worker, (10, 0.4))),
+    ]
+    print("Starting tasks in multiprocessing model")
+    mul_process_for_tasks(tasks=tasks)
+    print("Starting tasks not in multiprocessing model")
+    uni_process_for_tasks(tasks=tasks)
